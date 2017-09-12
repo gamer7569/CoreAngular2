6b12f4c2b1be14775397a8dd5e82c3013018eb8e
Kate Sky <kateskytech@gmail.com>
2017-09-12 12:56:30 -0500

    new repo

---
+++ app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about.component/about.component';
+import { PairingComponent } from './pairing-component/pairing.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/pairing', pathMatch: 'full' },
+    { path: 'pairing',  component: PairingComponent },
+    { path: 'about', component: AboutComponent },
+  ];
+@NgModule({
+    bootstrap: [ AppComponent ],
+    imports: [ BrowserModule,RouterModule.forRoot(routes), ],
+    declarations: [ 
+        AppComponent, 
+        PairingComponent, 
+        AboutComponent,  ],
+})
+export class AppModule { }
